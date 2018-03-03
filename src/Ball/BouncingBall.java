85010743dea943904eb563c4e03bf268a7b95b1c
alexei0tereshchenko <alexei0tereshchenko@gmail.com>
2018-03-04 02:45:48 +0300

    Added bricks

--- src/Ball/BouncingBall.java
+++ src/Ball/BouncingBall.java
@@ -1,11 +1,92 @@
 package Ball;
 
 import Frame.Field;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+public class BouncingBall implements Runnable {
+    private static final int MAX_RADIUS = 40;
+    private static final int MIN_RADIUS = 3;
+    private static final int MAX_SPEED = 10;
+    private Field field;
+    private int radius;
+    private Color color;
+    private double x;
+    private double y;
+    private int speed;
+    private double speedX;
+    private double speedY;
+    public BouncingBall(Field field) {
+        this.field = field;
+        radius = new Double(Math.random()*(MAX_RADIUS -
+                MIN_RADIUS)).intValue() + MIN_RADIUS;
 
-import javax.swing.*;
+        speed = new Double(Math.round(5*MAX_SPEED / radius)).intValue();
+        if (speed>MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
 
-public class BouncingBall extends JPanel {
-    public BouncingBall(Field field){
-        //ToDo: дописать конструктор
+        double angle = Math.random()*2*Math.PI;
+
+        speedX = 3*Math.cos(angle);
+        speedY = 3*Math.sin(angle);
+        color = new Color((float)Math.random(), (float)Math.random(),
+                (float)Math.random());
+
+        x = Math.random()*(field.getSize().getWidth()-2*radius) + radius;
+        y = Math.random()*(field.getSize().getHeight()-2*radius) + radius;
+
+        Thread thisThread = new Thread(this);
+        thisThread.start();
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                field.canMove(this);
+                if (x + speedX <= radius) {
+                    // Достигли левой стенки, отскакиваем право
+                    speedX = -speedX;
+                    x = radius;
+                } else
+                if (x + speedX >= field.getWidth() - radius) {
+                    // Достигли правой стенки, отскок влево
+                    speedX = -speedX;
+                    x=new Double(field.getWidth()-radius).intValue();
+                } else
+                if (y + speedY <= radius) {
+                    // Достигли верхней стенки
+                    speedY = -speedY;
+                    y = radius;
+                } else
+                if (y + speedY >= field.getHeight() - radius) {
+                    // Достигли нижней стенки
+                    speedY = -speedY;
+                    y=new Double(field.getHeight()-radius).intValue();
+                } else {
+                    // Просто смещаемся
+                    x += speedX;
+                    y += speedY;
+                }
+                //TODO: столкновения с кирпичами
+                /* Засыпаем на X миллисекунд, где X определяется
+                исходя из скорости
+                Скорость = 1 (медленно), засыпаем на 15 мс.
+                Скорость = 15 (быстро), засыпаем на 1 мс.*/
+                Thread.sleep(16-speed);
+            }
+        } catch (InterruptedException ex) {
+
+        }
+    }
+
+    public void paint(Graphics2D canvas) {
+        canvas.setColor(color);
+        canvas.setPaint(color);
+        Ellipse2D.Double ball = new Ellipse2D.Double(x-radius, y-radius,
+                2*radius, 2*radius);
+
+        canvas.draw(ball);
+        canvas.fill(ball);
     }
 }
--- src/Frame/Field.java
+++ src/Frame/Field.java
@@ -1,40 +1,68 @@
 package Frame;
 
 import Ball.BouncingBall;
+import Brick.SolidBrick;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
+
+@SuppressWarnings("serial")
 public class Field extends JPanel {
-    private ArrayList<BouncingBall> balls;
+
     private boolean paused;
 
-    public Field(){
-        setBackground(Color.white);
-        repaintTimer.start();
-    }
+    private BouncingBall ball;
+
+    private ArrayList<SolidBrick> bricks = new ArrayList<SolidBrick>(10);
+
     private Timer repaintTimer = new Timer(10, new ActionListener() {
+        @Override
         public void actionPerformed(ActionEvent ev) {
-// Задача обработчика события ActionEvent одна - перерисовка окна
             repaint();
         }
     });
 
-    public void addBall() {
-        balls.add(new BouncingBall(this));
+    public Field() {
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+        ball = new BouncingBall(this);
     }
 
-    // Унаследованный от JPanel метод перерисовки компонента
+    @Override
     public void paintComponent(Graphics g) {
-// Вызвать версию метода, унаследованную от предка
         super.paintComponent(g);
         Graphics2D canvas = (Graphics2D) g;
-// Последовательно запросить прорисовку от всех мячей, хранимых в списке
-        for (BouncingBall ball : balls) {
-            ball.paint(canvas);
+        ball.paint(canvas);
+        for(SolidBrick brick: bricks)
+        {
+            brick.paint(canvas);
+        }
+    }
+
+    public void addBrick(){
+        bricks.add(new SolidBrick(this));
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+            wait();
         }
     }
 }
